da3958988d802e8853d43d1ca0afb60148222d4d
Daniel Kremer <dkremer@1-D-3.42heilbronn.de>
2024-06-05 19:45:10 +0200

    added functions to check ifphilois deador not and if he shoulddie

---
+++ monitring.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitring.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkremer <dkremer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 14:53:45 by dkremer           #+#    #+#             */
+/*   Updated: 2024/06/04 18:32:19 by dkremer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	time_check(t_data *data)
+{
+	t_philo	*philo_d;
+	int		i;
+	long	now;
+
+	i = 0;
+	philo_d = data->philo;
+	while (i < data->philo_n)
+	{
+		pthread_mutex_lock(&data->eat);
+		now = get_current_time();
+		if (now - philo_d[i].last_eat >= \
+				data->t_die && \
+			philo_d[i].eating == false)
+		{
+			pthread_mutex_unlock(&data->eat);
+			philo_msg("died", &philo_d[i], philo_d[i].id);
+			return (1);
+		}
+		pthread_mutex_unlock(&data->eat);
+		i++;
+	}
+	return (0);
+}
+
+int	meal_check(t_data *data)
+{
+	t_philo	*philo_d;
+	int		i;
+	int		all_ate;
+
+	i = 0;
+	all_ate = 0;
+	philo_d = data->philo;
+	if (data->m_count == 0)
+		return (0);
+	while (i < data->philo_n)
+	{
+		pthread_mutex_lock(&data->eat);
+		if (philo_d[i].meal_c >= data->m_count)
+			all_ate++;
+		pthread_mutex_unlock(&data->eat);
+		i++;
+	}
+	if (all_ate == data->philo_n)
+		return (1);
+	return (0);
+}
+
+void	monitoring(t_data *data)
+{
+	while (data->died == false)
+	{
+		if (time_check(data) || meal_check(data))
+		{
+			pthread_mutex_lock(&data->dead);
+			data->died = true;
+			pthread_mutex_unlock(&data->dead);
+			break ;
+		}
+	}
+}
+
+int	work(t_data *data)
+{
+	int		i;
+	t_philo	*philo_data;
+
+	i = 0;
+	while (i < data->philo_n)
+	{
+		philo_data = &data->philo[i];
+		if (pthread_create(&philo_data->thread, NULL, &philo, philo_data))
+			return (free_and_exit(data), error("THREAD ERROR2!\n"));
+		i++;
+	}
+	monitoring(data);
+	i = 0;
+	while (i < data->philo_n)
+	{
+		philo_data = &data->philo[i];
+		if (pthread_join(philo_data->thread, NULL))
+			return (free_and_exit(data), error("THREAD ERROR3!\n"));
+		i++;
+	}
+	free_and_exit(data);
+	return (0);
+}
